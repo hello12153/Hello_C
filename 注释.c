906b6efd62d7c353e084b87332387802a0538c1c
陈明歌 <37860962+hello12153@users.noreply.github.com>
2021-12-15 08:46:04 +0800

    Create 注释.c
    注释

---
+++ 注释.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*
+* @Author: hello_chen
+* @Description: 
+* @Versions：1.0
+*/
+
+/*
+* 注释：向读者标注代码的作用，让读者更好解析代码
+* 
+*/
+
+#include<stdio.h>
+
+int note();
+
+int note()
+{
+	//两种风格都可以用，看使用场景
+	//C风格
+	/*块注释，缺点：不可以嵌套注释*/
+	
+	//C++风格
+	//行注释，可以注释一行也可以注释多行
+}

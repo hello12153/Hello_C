5080ae29d808f93cb3e12551a12f943116eea3c2
陈明歌 <37860962+hello12153@users.noreply.github.com>
2021-12-16 09:08:28 +0800

    Update 循环语句-练习.c

--- 循环语句-练习.c
+++ 循环语句-练习.c
@@ -3,6 +3,8 @@
 #include<string.h>
 #include<windows.h>
 #include<stdlib.h>
+//用于数学计算库
+#include<math.h>
 
 void Circulation_test01();
 void Circulation_test02();
@@ -95,4 +97,142 @@ void Circulation_test04()
 		left++,right--;
 
 	}
+}
+
+void Circulation_test05()
+{
+	char name[] = "chen";
+	char password[] = "123";
+
+	char name_test[] = "";
+	char password_test[] = "";
+
+	int ch;
+
+	int i = 0;
+
+	while (i>3)
+	{
+		ch = getchar();
+		printf("name:");
+		scanf("%s", name_test);
+		ch = getchar();
+		printf("password:");
+		scanf("%s", password_test);
+
+		if (strcmp)
+		{
+
+		}
+	}
+}
+
+void Circulation_test06()
+{
+	int a = 0, b = 0, c = 0;
+	int tmp = 0;
+	printf("请输入三个要判断大小的数:");
+	scanf("%d%d%d", &a, &b, &c);
+	if (a<b)
+	{
+		tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if (a<c)
+	{
+		tmp = a;
+		a = c;
+		c = tmp;
+	}
+	if (b<c)
+	{
+		tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("a:%d,b:%d,c:%d",a,b,c);
+}
+
+void Circulation_test07()
+{
+	for (size_t i = 0; i <= 100; i++)
+	{
+		//%取余数，/取整数
+		if (i % 3 == 0)
+		{
+			printf("%d ,", i);
+		}
+
+	}
+}
+
+//辗转相除法，求公约数
+void Circulation_test08()
+{
+	int a = 0;
+	int b = 0;
+	int r = 0;
+	printf("请输入要求公约数的两个数：");
+	/*scanf("%d", &a);
+	scanf("%d", &b);*/
+	scanf("%d%d",&a, &b);
+	//如果a%b不为0，进入while循环，否则不执行while
+	while ( r = a % b)
+	{
+		a = b;
+		b = r;
+	}
+	printf("最大公约数：%d",b);
+}
+
+//闰年
+void Circulation_test09() 
+{
+	int cont = 0;
+	for (int year = 1000; year <= 2000; year++)
+	{
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+		{
+			cont++;
+			printf("%d  ", year);
+		}
+
+	}
+	printf("cont = %d", cont);
+}
+
+//素数
+void Circulation_test10()
+{
+	for (int i = 101; i <= 200; i+=2)
+	{
+		int j = 0;
+		for (j = 2; j < sqrt(i); j++) 
+		{
+			if (i%j == 0)
+			{
+				break;
+			}
+		}
+		if (j>sqrt(i))
+		{
+			printf("%d  ", i);
+		}
+	}
+}
+
+//
+void Circulation_test11() 
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 10 == 9)
+			count++;
+		if (i / 10 == 9)
+			count++;
+	}
+	printf("%d ", count);
 }
